64525747f2631a869e8d67a054621852ec2f24b7
IshanPoudel <ishan.poudel@mavs.uta.edu>
2022-08-23 13:23:00 -0500

    Added XPATH dependencies

--- News_Module/Get_News.py
+++ News_Module/Get_News.py
@@ -11,6 +11,6 @@ stocks = ['GOOG', 'AAPL', 'FB', 'BABA', 'AMZN', 'GE', 'AMD', 'WMT', 'BAC', 'GM',
 
 for stock in stocks:
     get_news(stock)
-    #get news and store it in a database for further use.
+    #get news and store it in a databasGOOGe for further use.
     #have a flask API that uses the enpoint to get the news.
     
--- News_Module/Scrape.py
+++ News_Module/Scrape.py
@@ -1,3 +1,4 @@
+import re
 import time
 import logging
 import csv
@@ -8,6 +9,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import *
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+import mysql.connector
+import traceback
 
 
 
@@ -23,7 +30,8 @@ def get_news_from_google(key):
 
     # change space to a + sign as used by google url
     key = key.lower().replace(" ", "-")
-    url = "https://www.google.com/search?q=" + key + "&source=lmns&tbm=nws&bih=717&biw=1309&rlz=1C5CHFA_enNP906NP906&hl=en-US&sa=X&ved=2ahUKEwjzs8Wsnpz3AhUM0FMKHUsmADsQ_AUoAXoECAEQAQ"
+    # url = "https://www.google.com/search?q=" + key + "&source=lmns&tbm=nws&bih=717&biw=1309&rlz=1C5CHFA_enNP906NP906&hl=en-US&sa=X&ved=2ahUKEwjzs8Wsnpz3AhUM0FMKHUsmADsQ_AUoAXoECAEQAQ"
+    url = "https://www.google.com/search?q="+key+"++stock+news&biw=1309&bih=746&tbs=qdr%3Aw%2Csbd%3A1&tbm=nws&ei=HhQFY4_xEdKjqtsP5bmAkAY&ved=0ahUKEwiP-Ozxw935AhXSkWoFHeUcAGIQ4dUDCA4&uact=5&oq=UAA++stock+news&gs_lcp=Cgxnd3Mtd2l6LW5ld3MQAzIFCAAQkQIyBAgAEB4yBQgAEIYDMgUIABCGAzIFCAAQhgMyBQgAEIYDOgUIABCABDoGCAAQHhAHUABYlQNg5QVoAHAAeACAAVqIAb8CkgEBNJgBAKABAcABAQ&sclient=gws-wiz-news"
     driver.get(url)
 
     time.sleep(10)
@@ -37,32 +45,44 @@ def get_news_from_google(key):
 
     soup = BeautifulSoup(html_text, 'html.parser')
 
-    us_news_blob = soup.find_all("div", {"id": "search"})
 
 
-    for news in us_news_blob:
 
-        news_class = news.find("div", {"class": "v7W49e"})
-        # Iterate through <div id="search">
-        #                      <div class="v7W49e" >
-        #                           <div hase="i" >
-        news_hve_id = news_class.find_all("g-card")
-        print(news_hve_id)
+    XPATH_TUPLE_FOR_NEWS_TITLE  = (By.XPATH ,  "//*[@id='rso']/div/div/div[1]")
+    XPATH_TUPLE_FOR_NEWS_SUMMARY = (By.XPATH , "//*[@id='rso']/div/div/div[1]/div/a/div/div[2]/div[3]")
+    XPATH_TUPLE_FOR_NEWS_COMPANY = (By.XPATH , "//*[@id='rso']/div/div/div[1]/div/a/div/div[2]/div[1]/span")
+    XPATH_TUPLE_FOR_NEWS_LINK = (By.XPATH , "//*[@id='rso']/div/div/div[3]/div/a/div/div[1]/div/div")
 
-        for actual_news in news_hve_id:
+    data = driver.find_element(*XPATH_TUPLE_FOR_NEWS_CARD)
+    print(data.text)
+    html_for_house_cards = data.get_attribute('innnerHTML')
+    print(html_for_house_cards)
 
+    # // *[ @ id = "rso"] / div / div / div[1] / div / a / div / div[2] / div[2]
 
-            try:
-
-                news_text = actual_news.find("div", {"role": "heading"}).text
-                google_news_title_text.append(news_text)
-                news_title = actual_news.a['href']
-                google_news_link.append(news_title)
-                print("i added" + news_title)
-                driver.close()
-            except Exception as e:
-                print("Error")
-                driver.close()
+    # for news in us_news_blob:
+    #
+    #     news_class = news.find("div", {"class": "v7W49e"})
+    #     # Iterate through <div id="search">
+    #     #                      <div class="v7W49e" >
+    #     #                           <div hase="i" >
+    #     news_hve_id = news_class.find_all("g-card")
+    #     print(news_hve_id)
+    #
+    #     for actual_news in news_hve_id:
+    #
+    #
+    #         try:
+    #
+    #             news_text = actual_news.find("div", {"role": "heading"}).text
+    #             google_news_title_text.append(news_text)
+    #             news_title = actual_news.a['href']
+    #             google_news_link.append(news_title)
+    #             print("i added" + news_title)
+    #             driver.close()
+    #         except Exception as e:
+    #             print("Error")
+    #             driver.close()
     return  google_news_title_text , google_news_link
 
 
@@ -74,7 +94,5 @@ print("I am outside the function")
 for i in range(len(title)) :
 
     print("I am here")
-    print( title[i] , link[i])
-    print('\n')
 
-
+driver.close()
