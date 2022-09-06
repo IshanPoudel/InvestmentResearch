8e504f2ec4f8453323a6212283df1a8e3c1041a5
IshanPoudel <ishan.poudel@mavs.uta.edu>
2022-09-05 23:38:09 -0500

    Added user table for stock simulation function.

---
+++ Initialize_User_Database.py
@@ -0,0 +1,25 @@
+import mysql.connector
+import  json
+
+with open('config.json') as config_file:
+    data = json.load(config_file)
+
+db = mysql.connector.connect(host=data['host'],
+                             user=data['username'],
+                             passwd=data['password']
+                            )
+
+
+mycursor = db.cursor()
+
+mycursor.execute("DROP DATABASE IF EXISTS user_table")
+mycursor.execute("CREATE DATABASE stock_news")
+
+db = mysql.connector.connect(host=data['host'],
+                             user=data['username'],
+                             passwd=data['password'],
+                             db='user_table'
+                            )
+
+
+mycursor = db.cursor()
